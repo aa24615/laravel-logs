ca62aa281031ef04dbbaf28d374406b918a6eeb9
读心印 <839024615@qq.com>
2021-06-11 14:15:22 +0800

    中间件

--- src/Logs.php
+++ src/Logs.php
@@ -3,6 +3,7 @@
 namespace Zyan\LaravelLogs;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class Logs
 {
@@ -11,13 +12,27 @@ class Logs
     public function __construct()
     {
         $this->request = request();
+        $this->response = response();
     }
 
     public function request()
     {
         $content = $this->request->ip()." ".$this->request->method()." : ".$this->request->getUri();
         $this->appendContent($content);
-        $content = "DATA:".print_r($this->request->all(), true);
+
+        $content = 'userAgent:'.$this->request->getRequestFormat();
+        $this->appendContent($content);
+
+        $content = 'referer:'.(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '');
+        $this->appendContent($content);
+
+        $content = "ALL DATA:".print_r($this->request->all(), true);
+        $this->appendContent($content);
+
+        $content = "POST DATA:".print_r($this->request->post(), true);
+        $this->appendContent($content);
+
+        $content = "GET DATA:".print_r($this->request->query(), true);
         $this->appendContent($content);
 
         return $this;
@@ -32,6 +47,18 @@ class Logs
      */
     public function sql()
     {
+        $data    = [];
+
+        foreach (DB::getQueryLog() as $log) {
+            if (!array_key_exists($log['query'], $data)) {
+                $data[$log['query']] = 0;
+            }
+
+            ++ $data[$log['query']];
+        }
+        arsort($data);
+        $this->appendContent(var_export($data, true));
+
         return $this;
     }
 
@@ -44,6 +71,9 @@ class Logs
      */
     public function response()
     {
+        $body = $this->response->getContent();
+        $this->appendContent(print_r($body,true));
+
         return $this;
     }
 
@@ -77,8 +107,8 @@ class Logs
     public function write()
     {
         $disk = $this->getDisk();
-        $content = $this->br().$this->content;
-        $disk->info($content);
+        $this->appendContent($this->br());
+        $disk->info($this->content);
         $this->content = '';
     }
 
@@ -89,6 +119,6 @@ class Logs
 
     protected function br()
     {
-        return PHP_EOL.'---------------------------------------------------------------'.PHP_EOL;
+        return '---------------------------------------------------------------';
     }
 }
